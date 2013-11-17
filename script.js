ac34376938d63f6086ce85be28883603eb2fd128
vivianm <vivianmbaji@gmail.com>
2013-11-17 19:25:56 +0300

    Using break add a program that loops until it gets a correct answer

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ while (currentNumber <= 12) {
   alert(currentNumber);
     currentNumber = currentNumber + 2;
     }
+var answer;
+while (true) {
+  answer = prompt("You! What is the value of 2 + 2?", "");
+    if (answer == "4") {
+        alert("You must be a genius or something.");
+	    break;
+	      }
+	        else if (answer == "3" || answer == "5") {
+		    alert("Almost!");
+		      }
+		        else {
+			    alert("You're an embarrassment.");
+			      }
+			      }
