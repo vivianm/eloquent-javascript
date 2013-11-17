b2e18a8118bb3e0ed701492b6128f92c7ac5a617
vivianm <vivianmbaji@gmail.com>
2013-11-17 17:43:44 +0300

    Add a program that calculates and shows the value of 2 power 10

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ while (currentNumber <= 12) {
      alert(currentNumber);
        currentNumber = currentNumber + 2;
        }
+       var result = 1;
+       var counter = 0;
+       while (counter < 10) {
+         result = result * 2;
+	   counter = counter + 1;
+	   }
+	   show(result);
