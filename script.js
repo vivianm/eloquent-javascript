277e91dab29c629f784efaa4d78a365b04873ea5
vivianm <vivianmbaji@gmail.com>
2013-11-17 19:03:56 +0300

    Using for loop add a programm that shows all number below 20 but add two stars after them when they are greater than 15,one star when they are greater than 10 and no star otherwise

--- script.js
+++ script.js
@@ -44,8 +44,10 @@ while (currentNumber <= 12) {
 		       alert(counter);
 		       }
 		       for (var counter = 0; counter < 20; counter++) {
-		         if (counter % 4 == 0)
-			     alert(counter);
-			       else
-			           alert("(" + counter + ")");
-				   }
+		         if (counter > 15)
+			     alert(counter + "**");
+			       else if (counter > 10)
+			           alert(counter + "*");
+				     else
+				         alert(counter);
+					 }
