d7cf96b4b0211ee400e8cb590afc4a74ec598f42
vivianm <vivianmbaji@gmail.com>
2013-11-17 19:13:13 +0300

    Using promp add a program that ask for the value of 2+2 and gives you a different message if you give a correct, nearest answer or wrong answer

--- script.js
+++ script.js
@@ -51,3 +51,10 @@ while (currentNumber <= 12) {
 				     else
 				         alert(counter);
 					 }
+	var answer = prompt("You! What is the value of 2 + 2?", "");
+	if (answer == "4")
+	 alert("You must be a genius or something.");
+	  else if (answer == "3" || answer == "5")
+		   alert("Almost!");
+			    else
+		    alert("You're an embarrassment.");
