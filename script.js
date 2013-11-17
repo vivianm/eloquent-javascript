34586989b4cfbe549fb7353fa52303d9ab45d0c6
vivianm <vivianmbaji@gmail.com>
2013-11-17 19:28:47 +0300

    Using while add a program that loops until it gets a correct answer

--- script.js
+++ script.js
@@ -54,3 +54,13 @@ while (currentNumber <= 12) {
 	}
 	for (var current = 20; current % 7 != 0; current++);
 	  alert(current);
+	  var value = null;
+	  while (value != "4") {
+	    value = prompt("You! What is the value of 2 + 2?", "");
+	      if (value == "4")
+	          alert("You must be a genius or something.");
+		    else if (value == "3" || value == "5")
+		        alert("Almost!");
+			  else
+			      alert("You're an embarrassment.");
+			      }
