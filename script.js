b5ddfe8f2c2ae39a7f5dc6aea0ff84d799a5c5a1
vivianm <vivianmbaji@gmail.com>
2013-11-18 12:22:35 +0300

    Add different but similar functions,one adds 2 to its argument and one adds 5 to its argument

--- script.js
+++ script.js
@@ -85,3 +85,13 @@ function parentFunction() {
 
 			    var child = parentFunction();
 			    child();
+			    function makeAddFunction(amount) {
+			      function add(number) {
+			          return number + amount;
+				    }
+				      return add;
+				      }
+
+				      var addTwo = makeAddFunction(2);
+				      var addFive = makeAddFunction(5);
+				      alert(addTwo(1) + addFive(1));
