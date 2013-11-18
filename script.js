19506a924625b69b068f319c61d71ef77828eb5f
vivianm <vivianmbaji@gmail.com>
2013-11-19 00:23:02 +0300

    Add a program that gives range of numbers and their total(sum)

--- script.js
+++ script.js
@@ -555,3 +555,11 @@ function range(start, end) {
 
 		      alert(range(4));
 		      alert(range(2, 4));
+		      function sum(numbers) {
+		        var total = 0;
+			  for (var i = 0; i < numbers.length; i++)
+			      total += numbers[i];
+			        return total;
+				}
+
+				alert(sum(range(1, 10)));
