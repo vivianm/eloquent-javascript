6dcb32902f69d13559c31ca441c5dec7d7629bef
vivianm <vivianmbaji@gmail.com>
2013-11-18 18:24:05 +0300

    Add a program that will up the code

--- script.js
+++ script.js
@@ -370,3 +370,12 @@ for (var i = 0; i < mailArchive.length; i++) {
 										    alert("Good old Spot, may she rest in peace.");
 for (var cat in livingCats)
   alert(cat);
+  function addToSet(set, values) {
+    for (var i = 0; i < values.length; i++)
+        set[values[i]] = true;
+	}
+
+	function removeFromSet(set, values) {
+	  for (var i = 0; i < values.length; i++)
+	      delete set[values[i]];
+	      }
