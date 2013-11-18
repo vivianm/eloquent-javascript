0df1c25a20ae3d4f8b7b738ecd23263692d5e2e3
vivianm <vivianmbaji@gmail.com>
2013-11-18 23:55:26 +0300

    Example of cats add a program that stre birth-dates and later. the mother's name

--- script.js
+++ script.js
@@ -449,3 +449,15 @@ function findLivingCats() {
 						    }
 
 	alert(extractDate("died 27-04-2006: Black LeclÎáÎéÎıre"));
+	function catRecord(name, birthdate, mother) {
+	  return {name: name, birth: birthdate, mother: mother};
+	  }
+
+	  function addCats(set, names, birthdate, mother) {
+	    for (var i = 0; i < names.length; i++)
+	        set[names[i]] = catRecord(names[i], birthdate, mother);
+		}
+		function deadCats(set, names, deathdate) {
+		  for (var i = 0; i < names.length; i++)
+		      set[names[i]].death = deathdate;
+		      }
