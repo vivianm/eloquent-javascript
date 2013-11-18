067a77a0a2721e262c19765a832c0da86edf324a
vivianm <vivianmbaji@gmail.com>
2013-11-19 00:06:04 +0300

    Add a program that shows improved cats-algorithm

--- script.js
+++ script.js
@@ -474,3 +474,25 @@ function findLivingCats() {
        return string.slice(startAt, endAt);
        }
        alert(between("bu ] boo [ bah ] gzz", "[ ", " ]"));
+       function findCats() {
+         var mailArchive = retrieveMails();
+	   var cats = {"Spot": catRecord("Spot", new Date(1997, 2, 5),
+	                 "unknown")};
+
+			   function handleParagraph(paragraph) {
+			       if (startsWith(paragraph, "born"))
+addCats(cats, catNames(paragraph), extractDate(paragraph),
+				        extractMother(paragraph));
+	else if (startsWith(paragraph, "died"))
+ deadCats(cats, catNames(paragraph), extractDate(paragraph));
+							       }
+
+ for (var mail = 0; mail < mailArchive.length; mail++) {
+	var paragraphs = mailArchive[mail].split("\n");
+		 for (var i = 0; i < paragraphs.length; i++)
+	 handleParagraph(paragraphs[i]);
+									         }
+										   return cats;
+										   }
+
+										   var catData = findCats();
