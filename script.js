924ab71fb2ffa81213aef8d80595cbb784d65b8a
vivianm <vivianmbaji@gmail.com>
2013-11-18 11:32:38 +0300

    Add a program that gives the result of 2 exponent 10

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ function add(a, b) {
   }
 
   alert(add(2, 2));
+  function power(base, exponent) {
+    var result = 1;
+      for (var count = 0; count < exponent; count++)
+          result *= base;
+	    return result;
+	    }
+
+	    alert(power(2, 10));
