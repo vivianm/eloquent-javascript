3c210104c09192782204d7c3d90442853dcda77a
vivianm <vivianmbaji@gmail.com>
2013-11-18 15:35:56 +0300

    Add a program that used to guess the name of these properties

--- script.js
+++ script.js
@@ -287,3 +287,9 @@ alert(object3.value);
 var mailArchive = {"the first e-mail": "Dear nephew, ...",
                    "the second e-mail": "..."
 		                      /* and so on ... */};
+	var mailArchive = {0: "Dear nephew, ... (mail number 1)",
+		            1: "(mail number 2)",
+			    2: "(mail number 3)"};
+
+ for (var current = 0; current in mailArchive; current++)
+alert("Processing e-mail #", current, ": ", mailArchive[current]);
