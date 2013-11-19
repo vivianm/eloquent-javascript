e3ad2804e36c20b83a12ac17dc63a889947f3d89
vivianm <vivianmbaji@gmail.com>
2013-11-19 18:48:52 +0300

    Add a rendered emphasised tag fragments with is em, and normal text without any extra tags

--- script.js
+++ script.js
@@ -889,3 +889,16 @@ function footnote(number) {
   return tag("sup", [link("#footnote" + number,
                             String(number))]);
 			    }
+function renderParagraph(paragraph) {
+  return tag(paragraph.type, map(renderFragment,
+                                   paragraph.content));
+				   }
+
+				   function renderFragment(fragment) {
+				     if (fragment.type == "reference")
+				         return footnote(fragment.number);
+			else if (fragment.type == "emphasised")
+					 return tag("em", [fragment.content]);
+					  else if (fragment.type == "normal")
+						     return fragment.content;
+						     }
