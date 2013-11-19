f502a3cbdc0758a5c059f7819b8df3d9d10d37fb
vivianm <vivianmbaji@gmail.com>
2013-11-19 15:51:40 +0300

    Add a sum function which is a variant of an algorithm usually called reduce or fold

--- script.js
+++ script.js
@@ -692,3 +692,17 @@ function negate(func) {
 	      return !func.apply(null, arguments);
 	        };
 		}
+function reduce(combine, base, array) {
+  forEach(array, function (element) {
+      base = combine(base, element);
+        });
+	  return base;
+	  }
+
+	  function add(a, b) {
+	    return a + b;
+	    }
+
+	    function sum(numbers) {
+	      return reduce(add, 0, numbers);
+	      }
