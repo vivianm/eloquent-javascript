97a936f59f2f13124eb093d6f037930bfcfe55ca
vivianm <vivianmbaji@gmail.com>
2013-11-19 14:54:05 +0300

    Add a function which determines whether an object, and the objects stored inside it, contain at least seven true values

--- script.js
+++ script.js
@@ -632,3 +632,31 @@ try {
     alert("Caught: " + error.message);
     }
     throw new Error("Fire!");
+    var FoundSeven = {};
+
+    function hasSevenTruths(object) {
+      var counted = 0;
+
+        function count(object) {
+	    for (var name in object) {
+	          if (object[name] === true) {
+		          counted++;
+			          if (counted == 7)
+				            throw FoundSeven;
+					          }
+	else if (typeof object[name] == "object") {
+		count(object[name]);
+			}
+								          }
+}									    }
+}
+									      trtry {
+									          count(object);
+										      return false;
+										        }
+											  catch (exception) {
+											      if (exception != FoundSeven)
+											            throw exception;
+												        return true;
+													  }
+													  }
