cbb26c977cb1581c64c56fbde73f40d6f1d7fd83
vivianm <vivianmbaji@gmail.com>
2013-11-19 14:30:49 +0300

    Add a program that continues running at the point where the exception was caught

--- script.js
+++ script.js
@@ -586,3 +586,20 @@ if (parenthesized != undefined)
 	      }
 
 	      alert(lastElement([1, 2, undefined]));
+	      function lastElement(array) {
+	        if (array.length > 0)
+		    return array[array.length - 1];
+		      else
+		     throw "Can not take the last element of an empty array.";
+			  }
+
+			  function lastElementPlusTen(array) {
+			    return lastElement(array) + 10;
+			    }
+
+			    try {
+			      print(lastElementPlusTen([]));
+			      }
+			      catch (error) {
+			        alert("Something went wrong: ", error);
+				}
