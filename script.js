4023b8e1711e35137a122d42e248aa36e0de491c
vivianm <vivianmbaji@gmail.com>
2013-11-19 16:04:00 +0300

    Add a function count that produces equal result as with function countZeroes

--- script.js
+++ script.js
@@ -712,3 +712,16 @@ function countZeroes(array) {
         }
 	  return reduce(counter, 0, array);
 	  }
+function count(test, array) {
+  return reduce(function(total, element) {
+      return total + (test(element) ? 1 : 0);
+        }, 0, array);
+	}
+
+	function equals(x) {
+	  return function(element) {return x === element;};
+	  }
+
+	  function countZeroes(array) {
+	    return count(equals(0), array);
+	    }
