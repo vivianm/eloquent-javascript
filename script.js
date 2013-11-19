6c9f0bb9ad6f779e6197114e5cdabea4c1c9c20e
vivianm <vivianmbaji@gmail.com>
2013-11-19 17:48:09 +0300

    Add a function splitParagraph which, given a paragraph string, returns an array of paragraph fragments

--- script.js
+++ script.js
@@ -748,3 +748,41 @@ function processParagraph(paragraph) {
 			  alert(processParagraph(paragraphs[0]));
 var paragraphs = map(processParagraph,
                      recluseFile().split("\n\n"));
+		     function splitParagraph(text) {
+		       function indexOrEnd(character) {
+		           var index = text.indexOf(character);
+			       return index == -1 ? text.length : index;
+			         }
+
+				   function takeNormal() {
+				       var end = reduce(Math.min, text.length,
+				          map(indexOrEnd, ["*", "{"]));
+			var part = text.slice(0, end);
+			text = text.slice(end);
+					return part;
+									  }
+
+		function takeUpTo(character) {
+									        var end = text.indexOf(character, 1);
+										    if (end == -1)
+										          throw new Error("Missing closing '" + character + "'");
+											      var part = text.slice(1, end);
+											          text = text.slice(end + 1);
+												      return part;
+												        }
+
+													  var fragments = [];
+
+													    while (text != "") {
+													        if (text.charAt(0) == "*")
+														      fragments.push({type: "emphasised",
+														                 content: takeUpTo("*")});
+														else if (text.charAt(0) == "{")
+														fragments.push({type: "footnote",
+																		                            content: takeUpTo("}")});
+																					        else
+																						      fragments.push({type: "normal",
+																						                            content: takeNormal()});
+																									      }
+																									        return fragments;
+																										}
