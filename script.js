419faea3b6ebeb75adddd20ce09fa188a9f3edbf
vivianm <vivianmbaji@gmail.com>
2013-11-19 18:01:34 +0300

    Add a function extractFootnotes to take out the footnotes and put references to them in their place

--- script.js
+++ script.js
@@ -796,3 +796,26 @@ var paragraphs = map(processParagraph,
 																					        return {type: (header == 0 ? "p" : "h" + header),
 																						          content: splitParagraph(paragraph)};
 																							  }
+																				function extractFootnotes(paragraphs) {
+																				  var footnotes = [];
+																				    var currentNote = 0;
+
+																				      function replaceFootnote(fragment) {
+																				          if (fragment.type == "footnote") {
+																					        currentNote++;
+																						      footnotes.push(fragment);
+																						            fragment.number = currentNote;
+																							          return {type: "reference", number: currentNote};
+																								      }
+																								          else {
+																									        return fragment;
+																										    }
+																										      }
+
+																				forEach(paragraphs, function(paragraph) {
+																				 paragraph.content = map(replaceFootnote,
+																											                                paragraph.content);
+																															  });
+
+																															    return footnotes;
+																															    }     
