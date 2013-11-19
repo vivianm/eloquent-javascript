18897de0825760562b81cea63eb3380eecd3d705
vivianm <vivianmbaji@gmail.com>
2013-11-19 18:25:03 +0300

    Add recursive function to turn an HTML element object into a string

--- script.js
+++ script.js
@@ -844,3 +844,39 @@ var paragraphs = map(processParagraph,
 																																	        });
 																																		  return text;
 																																		  }
+																														function renderHTML(element) {
+																														  var pieces = [];
+
+																														    function renderAttributes(attributes) {
+																														        var result = [];
+																															    if (attributes) {
+																															          for (var name in attributes) 
+																																          result.push(" " + name + "=\"" +
+																																	          escapeHTML(attributes[name]) + "\"");
+																																			          }
+																																				      return result.join("");
+																																				        }
+
+																																	  function render(element) {
+																																					      // Text node
+																																if (typeof element == "string") {
+																																pieces.push(escapeHTML(element));
+																																							    }
+																																							        // Empty tag
+																																								    else if (!element.content || element.content.length == 0) {
+																																								          pieces.push("<" + element.name +
+																																									                    renderAttributes(element.attributes) + "/>");
+																																											        }
+																																												    // Tag with content
+																																												        else {
+																																													      pieces.push("<" + element.name +
+																																								 renderAttributes(element.attributes) + ">");
+																																										 forEach(element.content, render);
+																																											 pieces.push("</" + element.name + ">");
+																																																	        }
+																																																		  }
+
+																																																		    render(element);
+																																																		      return pieces.join("");
+																																																		      }
+
