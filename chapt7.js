1799737a7d6d4b5ff52ff78f97e3331d29379283
vivianm <vivianmbaji@gmail.com>
2013-11-20 15:58:47 +0300

    add an object that associates place-names with signpost lists

--- chapt7.js
+++ chapt7.js
@@ -1,3 +1,7 @@
 var roads = [{point1: "Point Kiukiu", point2: "Hanaiapa", length: 19},
              {point1: "Point Kiukiu", point2: "Mt Feani", length: 15}
 	                  /* and so on */];
+ var roads = {"Point Kiukiu": [{to: "Hanaiapa", distance: 19},
+  {to: "Mt Feani", distance: 15},
+     {to: "Taaoa", distance: 15}],
+        "Taaoa": [/* et cetera */]};
