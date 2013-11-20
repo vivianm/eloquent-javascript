c66ab1fde3911b93cba1474fd28750a9659d7004
vivianm <vivianmbaji@gmail.com>
2013-11-20 16:01:49 +0300

    add a program that first initializes object calld roads and add a function makeRoad

--- chapt7.js
+++ chapt7.js
@@ -5,3 +5,13 @@ var roads = [{point1: "Point Kiukiu", point2: "Hanaiapa", length: 19},
   {to: "Mt Feani", distance: 15},
      {to: "Taaoa", distance: 15}],
         "Taaoa": [/* et cetera */]};
+var roads = {};
+function makeRoad(from, to, length) {
+  function addRoad(from, to) {
+      if (!(from in roads))
+            roads[from] = [];
+	        roads[from].push({to: to, distance: length});
+		  }
+		    addRoad(from, to);
+		      addRoad(to, from);
+		      }
