87521aff2022b9a8b0ef2d4e3f4691e77b437baa
vivianm <vivianmbaji@gmail.com>
2013-11-20 18:29:40 +0300

    Add a function shortestRouter to find the shortest

--- chapt7.js
+++ chapt7.js
@@ -145,3 +145,11 @@ function possibleRoutes(from, to) {
 
 										show(possibleRoutes("Point Teohotepapapa", "Point Kiukiu").length);
 											show(possibleRoutes("Hanapaoa", "Mt Ootua"));
+function shortestRoute(from, to) {
+  var currentShortest = null;
+    forEach(possibleRoutes(from, to), function(route) {
+        if (!currentShortest || currentShortest.length > route.length)
+	      currentShortest = route;
+	        });
+		  return currentShortest;
+		  }
