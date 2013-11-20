4a18fc450f68c2d0c85028d1da3a385ad07aa31a
vivianm <vivianmbaji@gmail.com>
2013-11-20 19:24:21 +0300

    Finally actual path-finding function

--- chapt7.js
+++ chapt7.js
@@ -267,3 +267,39 @@ function pointID(point) {
       function findReached(list, point) {
         return list[pointID(point)];
 	}
+	function findRoute(from, to) {
+	  var open = new BinaryHeap(routeScore);
+	    var reached = makeReachedList();
+
+	      function routeScore(route) {
+	          if (route.score == undefined)
+		        route.score = estimatedDistance(route.point, to) +
+			                    route.length;
+					        return route.score;
+						  }
+		function addOpenRoute(route) {
+				open.push(route);
+			storeReached(reached, route.point, route);
+							      }
+			addOpenRoute({point: from, length: 0});
+
+			while (open.size() > 0) {
+			 var route = open.pop();
+			if (samePoint(route.point, to))
+									        return route;
+										    
+										        forEach(possibleDirections(route.point), function(direction) {
+											      var known = findReached(reached, direction);
+											            var newLength = route.length +
+												                     weightedDistance(route.point, direction);
+													if (!known || known.length > newLength){
+																   if (known)
+																open.remove(known);        
+														addOpenRoute({point: direction,
+																			                        from: route,
+																						                      length: newLength});
+																								            }
+																									        });
+																										  }
+																										    return null;
+																										    }
