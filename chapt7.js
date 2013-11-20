72a064e3e43a01f21f4a47d605a1ddad46315f64
vivianm <vivianmbaji@gmail.com>
2013-11-20 17:14:17 +0300

    add a function makeRoads that takes any uneven number of arguments

--- chapt7.js
+++ chapt7.js
@@ -19,3 +19,15 @@ function makeRoads(start) {
   for (var i = 1; i < arguments.length; i += 2)
       makeRoad(start, arguments[i], arguments[i + 1]);
       }
+var roads = {};
+makeRoads("Point Kiukiu", "Hanaiapa", 19,
+          "Mt Feani", 15, "Taaoa", 15);
+	  makeRoads("Airport", "Hanaiapa", 6, "Mt Feani", 5,
+	            "Atuona", 4, "Mt Ootua", 11);
+		    makeRoads("Mt Temetiu", "Mt Feani", 8, "Taaoa", 4);
+		    makeRoads("Atuona", "Taaoa", 3, "Hanakee pearl lodge", 1);
+	 makeRoads("Cemetery", "Hanakee pearl lodge", 6, "Mt Ootua", 5);
+		    makeRoads("Hanapaoa", "Mt Ootua", 3);
+	  makeRoads("Puamua", "Mt Ootua", 13, "Point Teohotepapapa", 14);
+
+		    show(roads["Airport"]);
