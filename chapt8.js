70697d160fcbff4a25123e84ee95788702dff495
vivianm <vivianmbaji@gmail.com>
2013-11-21 13:33:15 +0300

    Add function speak with variable this

---
+++ chapt8.js
@@ -0,0 +1,15 @@
+var rabbit = {};
+rabbit.speak = function(line) {
+  alert("The rabbit says '", line, "'");
+  };
+
+  rabbit.speak("Well, now you're asking me.");
+
+  function speak(line) {
+    alert("The ", this.adjective, " rabbit says '", line, "'");
+    }
+    var whiteRabbit = {adjective: "white", speak: speak};
+    var fatRabbit = {adjective: "fat", speak: speak};
+
+    whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
+    fatRabbit.speak("I could sure use a carrot right now.");
