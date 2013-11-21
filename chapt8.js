111f7b2541413b3e5b711d9b3955e09343af4eb1
vivianm <vivianmbaji@gmail.com>
2013-11-21 14:52:31 +0300

    Give objects properties method

--- chapt8.js
+++ chapt8.js
@@ -73,3 +73,13 @@ function Rabbit(adjective) {
 var noCatsAtAll = {};
 if ("constructor" in noCatsAtAll)
   alert("Yes, there definitely is a cat called 'constructor'.");
+
+Object.prototype.properties = function() {
+  var result = [];
+    for (var property in this)
+        result.push(property);
+	  return result;
+	  };
+
+	  var test = {x: 10, y: 3};
+	  show(test.properties());
