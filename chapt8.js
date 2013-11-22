c18abf98e25e9948619436745154525449609aee
vivianm <vivianmbaji@gmail.com>
2013-11-22 15:29:09 +0300

    Add a method toString to the Terrarium prototype

--- chapt8.js
+++ chapt8.js
@@ -308,3 +308,15 @@ function characterFromElement(element) {
 	    }
 
 	    show(characterFromElement(wall));
+
+
+Terrarium.prototype.toString = function() {
+  var characters = [];
+    var endOfLine = this.grid.width - 1;
+      this.grid.each(function(point, value) {
+          characters.push(characterFromElement(value));
+	      if (point.x == endOfLine)
+	            characters.push("\n");
+		      });
+		        return characters.join("");
+			};
