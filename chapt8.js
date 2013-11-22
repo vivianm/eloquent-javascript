aaedb974e1b11b5af69777c8d98e1942bda3b78f
vivianm <vivianmbaji@gmail.com>
2013-11-22 15:13:22 +0300

    Looking direction

--- chapt8.js
+++ chapt8.js
@@ -248,3 +248,19 @@ testGrid.setValueAt(new Point(1, 1), "o");
 testGrid.each(function(point, value) {
   alert(point.x, ",", point.y, ": ", value);
   });
+
+
+var directions = new Dictionary(
+  {"n":  new Point( 0, -1),
+     "ne": new Point( 1, -1),
+        "e":  new Point( 1,  0),
+	   "se": new Point( 1,  1),
+	      "s":  new Point( 0,  1),
+	         "sw": new Point(-1,  1),
+		    "w":  new Point(-1,  0),
+		       "nw": new Point(-1, -1)});
+
+		       show(new Point(4, 4).add(directions.lookup("se")));
+
+
+
