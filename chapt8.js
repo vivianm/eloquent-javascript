9c3b86dabc87e72bce820fb4eff2047a22cb25da
vivianm <vivianmbaji@gmail.com>
2013-11-22 15:19:45 +0300

    Add Trrarium object

--- chapt8.js
+++ chapt8.js
@@ -267,3 +267,28 @@ function StupidBug() {};
 StupidBug.prototype.act = function(surroundings) {
   return {type: "move", direction: "s"};
   };
+
+
+
+var wall = {};
+
+function Terrarium(plan) {
+  var grid = new Grid(plan[0].length, plan.length);
+    for (var y = 0; y < plan.length; y++) {
+        var line = plan[y];
+	    for (var x = 0; x < line.length; x++) {
+	          grid.setValueAt(new Point(x, y),
+		                        elementFromCharacter(line.charAt(x)));
+					    }
+					      }
+					        this.grid = grid;
+						}
+
+	function elementFromCharacter(character) {
+	 if (character == " ")
+		return undefined;
+			else if (character == "#")
+			 return wall;
+				else if (character == "o")
+				 return new StupidBug();
+								  }
