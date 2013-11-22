d43e78fcceab90db0140d862681ade6307dad68a
vivianm <vivianmbaji@gmail.com>
2013-11-22 14:49:00 +0300

    Easier initialization of the array.new Array(x)

--- chapt8.js
+++ chapt8.js
@@ -209,3 +209,24 @@ var grid = [["0,0", "1,0", "2,0"],
 var grid = ["0,0", "1,0", "2,0",
             "0,1", "1,1", "2,1"];
 	    show(grid[2 + 1 * 3]);
+
+
+function Grid(width, height) {
+  this.width = width;
+    this.height = height;
+      this.cells = new Array(width * height);
+      }
+      Grid.prototype.valueAt = function(point) {
+        return this.cells[point.y * this.width + point.x];
+	};
+	Grid.prototype.setValueAt = function(point, value) {
+	  this.cells[point.y * this.width + point.x] = value;
+	  };
+	  Grid.prototype.isInside = function(point) {
+	    return point.x >= 0 && point.y >= 0 &&
+	             point.x < this.width && point.y < this.height;
+		     };
+		     Grid.prototype.moveValue = function(from, to) {
+		       this.setValueAt(to, this.valueAt(from));
+		         this.setValueAt(from, undefined);
+			 };
