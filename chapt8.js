3968e778693ac2ddc5afecf780f60a2ab8ebf4c1
vivianm <vivianmbaji@gmail.com>
2013-11-23 12:46:56 +0300

    adding create method to the object prototype

--- chapt8.js
+++ chapt8.js
@@ -721,3 +721,11 @@ function CleverLichenEater() {
 											  var strange = new StrangeArray();
 											  strange.push(4);
 											  show(strange);
+
+									
+Object.prototype.create = function() {
+  var object = clone(this);
+    if (typeof object.construct == "function")
+        object.construct.apply(object, arguments);
+	  return object;
+	  };
