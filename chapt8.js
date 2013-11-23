d6bc8451ac74bf7fdcf3caf470c3151bb2e367af
vivianm <vivianmbaji@gmail.com>
2013-11-23 11:04:07 +0300

    Take a random element from an array

--- chapt8.js
+++ chapt8.js
@@ -478,3 +478,11 @@ Dictionary.prototype.names = function() {
       };
 
       show(directions.names());
+
+function randomElement(array) {
+  if (array.length == 0)
+      throw new Error("The array is empty.");
+        return array[Math.floor(Math.random() * array.length)];
+	}
+
+	show(randomElement(["heads", "tails"]));
