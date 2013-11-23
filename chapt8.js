e0f35f8ce9fddacab7174764859197b16d578f06
vivianm <vivianmbaji@gmail.com>
2013-11-23 12:14:35 +0300

    modifying LichenEater function

--- chapt8.js
+++ chapt8.js
@@ -670,3 +670,36 @@ LichenEater.prototype.act = function(surroundings) {
 		        else
 			    return {type: "wait"};
 			    };
+
+
+function CleverLichenEater() {
+  this.energy = 10;
+    this.direction = "ne";
+    }
+    CleverLichenEater.prototype.act = function(surroundings) {
+      var emptySpace = findDirections(surroundings, " ");
+        var lichen = findDirections(surroundings, "*");
+
+	  if (this.energy >= 30 && emptySpace.length > 0) {
+	      return {type: "reproduce",
+	                  direction: randomElement(emptySpace)};
+			    }
+			      else if (lichen.length > 1) {
+			          return {type: "eat",
+				              direction: randomElement(lichen)};
+					        }
+			 else if (emptySpace.length > 0) {
+			 if (surroundings[this.direction] != " ")
+		this.direction = randomElement(emptySpace);
+			return {type: "move",
+			direction: this.direction};
+									      }
+				   else {
+										    return {type: "wait"};
+										      }
+										      };
+										      CleverLichenEater.prototype.character = "c";
+
+										      creatureTypes.register(CleverLichenEater);
+
+										      
