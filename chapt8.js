74cf62f22ed0f0205772e4cf86b5fef1ffd8248f
vivianm <vivianmbaji@gmail.com>
2013-11-23 08:23:15 +0300

    Add a method listSurroundings to the Terrarium prototype

--- chapt8.js
+++ chapt8.js
@@ -354,3 +354,17 @@ Terrarium.prototype.listActingCreatures = function() {
 	        });
 		  return found;
 		  };
+
+
+Terrarium.prototype.listSurroundings = function(center) {
+  var result = {};
+    var grid = this.grid;
+      directions.each(function(name, direction) {
+          var place = center.add(direction);
+	      if (grid.isInside(place))
+	            result[name] = characterFromElement(grid.valueAt(place));
+		        else
+			      result[name] = "#";
+			        });
+				  return result;
+				  };
