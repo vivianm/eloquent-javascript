8532ab158b042e080edb04ea1bf6964c3828b55a
vivianm <vivianmbaji@gmail.com>
2013-11-23 12:07:16 +0300

    Add new act method for lichen

--- chapt8.js
+++ chapt8.js
@@ -655,4 +655,18 @@ var lichenPlan =
 
 
 
-terrarium.stop();				   
+terrarium.stop();
+
+LichenEater.prototype.act = function(surroundings) {
+  var emptySpace = findDirections(surroundings, " ");
+    var lichen = findDirections(surroundings, "*");
+
+      if (this.energy >= 30 && emptySpace.length > 0)
+          return {type: "reproduce", direction: randomElement(emptySpace)};
+	    else if (lichen.length > 1)
+	        return {type: "eat", direction: randomElement(lichen)};
+		  else if (emptySpace.length > 0)
+		   return {type: "move", direction: randomElement(emptySpace)};
+		        else
+			    return {type: "wait"};
+			    };
