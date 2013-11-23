dad2d7f4df4a7eb8bbd65bfe35de1012a2954de6
vivianm <vivianmbaji@gmail.com>
2013-11-23 13:18:29 +0300

    Add fake constructor and apply instanceof to it

--- chapt8.js
+++ chapt8.js
@@ -786,3 +786,13 @@ var SmallItem = Item.extend({
 
 		    var pencil = SmallItem.create("the red pencil");
 		    pencil.take();
+
+
+Object.prototype.hasPrototype = function(prototype) {
+  function DummyConstructor() {}
+    DummyConstructor.prototype = prototype;
+      return this instanceof DummyConstructor;
+      };
+
+      show(pencil.hasPrototype(Item));
+      show(pencil.hasPrototype(DetailedItem));
