132931782f24886cf6bbcef07ff7a58fbd63d598
vivianm <vivianmbaji@gmail.com>
2013-11-23 11:58:38 +0300

    Use findDirection and randomelement to check the surrondings and pick direction

--- chapt8.js
+++ chapt8.js
@@ -610,3 +610,25 @@ LifeLikeTerrarium.prototype.processCreature = function(creature) {
 														      });
 														        return found;
 															}
+
+
+							
+function LichenEater() {
+  this.energy = 10;
+  }
+  LichenEater.prototype.act = function(surroundings) {
+    var emptySpace = findDirections(surroundings, " ");
+      var lichen = findDirections(surroundings, "*");
+
+        if (this.energy >= 30 && emptySpace.length > 0)
+	    return {type: "reproduce", direction: randomElement(emptySpace)};
+	      else if (lichen.length > 0)
+	          return {type: "eat", direction: randomElement(lichen)};
+		    else if (emptySpace.length > 0)
+		  return {type: "move", direction: randomElement(emptySpace)};
+			  else
+			      return {type: "wait"};
+			      };
+			      LichenEater.prototype.character = "c";
+
+			      creatureTypes.register(LichenEater);
