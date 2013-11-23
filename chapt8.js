498fea55f9daa875a75f409be6872162e7605abf
vivianm <vivianmbaji@gmail.com>
2013-11-23 13:28:52 +0300

    Add smallItem prototype in Mixinto function

--- chapt8.js
+++ chapt8.js
@@ -796,3 +796,19 @@ Object.prototype.hasPrototype = function(prototype) {
 
       show(pencil.hasPrototype(Item));
       show(pencil.hasPrototype(DetailedItem));
+
+
+function mixInto(object, mixIn) {
+  forEachIn(mixIn, function(name, value) {
+      object[name] = value;
+        });
+	};
+
+	var SmallDetailedItem = clone(DetailedItem);
+	mixInto(SmallDetailedItem, SmallItem);
+
+	var deadMouse = SmallDetailedItem.create(
+	  "Fred the mouse",
+	    "he is dead");
+	    deadMouse.inspect();
+	    deadMouse.kick();
