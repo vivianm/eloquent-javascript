e79c043ac32459aa6439b1b6a5640f77c2bf5df0
vivianm <vivianmbaji@gmail.com>
2013-11-25 14:53:24 +0300

    Add first variant of dependency management

--- chapt9.js
+++ chapt9.js
@@ -1,2 +1,6 @@
 load("FunctionalTools.js");
 eval("alert(\"I am a string inside a string!\");");
+var dependencies =
+  {"ObjectTools.js": ["FunctionalTools.js"],
+     "Dictionary.js":  ["ObjectTools.js"],
+        "TestModule.js":  ["FunctionalTools.js", "Dictionary.js"]};
