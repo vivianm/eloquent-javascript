45938d772799456f6497095a61db9ba34c442901
vivianm <vivianmbaji@gmail.com>
2013-11-25 18:42:50 +0300

    Extracting mark-up from a document

--- chapt10.js
+++ chapt10.js
@@ -1,2 +1,7 @@
 var slash = /\//;
 show("AC/DC".search(slash));
+
+var asteriskOrBrace = /[\{\*]/;
+var story =
+  "We noticed the *giant sloth*, hanging from a giant branch.";
+  show(story.search(asteriskOrBrace));
