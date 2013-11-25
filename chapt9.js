9966b04046c300d2476e9a0066e462c6099abceb
vivianm <vivianmbaji@gmail.com>
2013-11-25 15:02:59 +0300

    Loading file dependency

--- chapt9.js
+++ chapt9.js
@@ -4,3 +4,17 @@ var dependencies =
   {"ObjectTools.js": ["FunctionalTools.js"],
      "Dictionary.js":  ["ObjectTools.js"],
         "TestModule.js":  ["FunctionalTools.js", "Dictionary.js"]};
+
+var loadedFiles = {};
+
+function require(file) {
+  if (dependencies[file]) {
+      var files = dependencies[file];
+          for (var i = 0; i < files.length; i++)
+	        require(files[i]);
+		  }
+		    if (!loadedFiles[file]) {
+		        loadedFiles[file] = true;
+			    load(file);
+			      }
+			      }
