74124ff5fdf714cf9e4b11f38032db9ddd13650e
vivianm <vivianmbaji@gmail.com>
2013-11-25 18:50:23 +0300

    Shortcuts for sets of characters

--- chapt10.js
+++ chapt10.js
@@ -5,3 +5,6 @@ var asteriskOrBrace = /[\{\*]/;
 var story =
   "We noticed the *giant sloth*, hanging from a giant branch.";
   show(story.search(asteriskOrBrace));
+
+  var digitSurroundedBySpace = /\s\d\s/;
+  show("1a 2 3d".search(digitSurroundedBySpace));
