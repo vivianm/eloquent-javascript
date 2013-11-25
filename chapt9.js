8dfe8ca40692005fa4981b5ca4fd9b4977cda468
vivianm <vivianmbaji@gmail.com>
2013-11-25 16:40:44 +0300

    Passing arguments to function by wrapping them

--- chapt9.js
+++ chapt9.js
@@ -106,3 +106,24 @@ function range(start, end, stepSize, length) {
 		      }
 
 		      show(range(0, undefined, 4, 5));
+
+
+function defaultTo(object, values) {
+  forEachIn(values, function(name, value) {
+      if (!object.hasOwnProperty(name))
+            object[name] = value;
+	      });
+	      }
+
+	      function range(args) {
+	        defaultTo(args, {start: 0, stepSize: 1});
+		  if (args.end == undefined)
+		      args.end = args.start + args.stepSize * (args.length - 1);
+
+		        var result = [];
+	for (; args.start <= args.end; args.start += args.stepSize)
+			      result.push(args.start);
+			        return result;
+				}
+
+				show(range({stepSize: 4, length: 5}));
