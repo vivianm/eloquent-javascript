fd4b7e884e1a47a896477b5ff004c456607bd1dc
vivianm <vivianmbaji@gmail.com>
2013-11-26 17:44:47 +0300

    Looking for dates in which the day or month are written with only one digit.

--- chapt10.js
+++ chapt10.js
@@ -43,3 +43,13 @@ show(holyCow.test("Sacred bovine!"));
 show("No".match(/Yes/));
 show("... yes".match(/yes/));
 show("Giant Ape".match(/giant (\w+)/i));
+
+function extractDate(string) {
+  var found = string.match(/(\d\d?)\/(\d\d?)\/(\d{4})/);
+    if (found == null)
+        throw new Error("No date found in '" + string + "'.");
+	  return new Date(Number(found[3]), Number(found[2]) - 1,
+	                    Number(found[1]));
+			    }
+
+show(extractDate("born 5/2/2007 (mother Noog): Long-ear Johnson"));
