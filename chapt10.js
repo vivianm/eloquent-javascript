1758c85cb066d6d4904d891da07945e69bfe8146
vivianm <vivianmbaji@gmail.com>
2013-11-26 17:12:03 +0300

    Pattern starting at the beginning of a string

--- chapt10.js
+++ chapt10.js
@@ -13,3 +13,6 @@ var story =
 
   var datePattern = /\d\d\/\d\d\/\d\d\d\d/;
   show("born 15/11/2003 (mother Spot): White Fang".search(datePattern));
+
+  show(/a+/.test("blah"));
+  show(/^a+$/.test("blah"));
