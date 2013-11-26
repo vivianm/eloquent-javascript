ed08986598dd52954ce008a2955fb04a07fdd5d7
vivianm <vivianmbaji@gmail.com>
2013-11-26 17:55:08 +0300

    Replacing pieces of a string

--- chapt10.js
+++ chapt10.js
@@ -57,3 +57,20 @@ alert("Borobudur".replace(/[ou]/g, "a"));
 
 var names = "Picasso, Pablo\nGauguin, Paul\nVan Gogh, Vincent";
 alert(names.replace(/([\w ]+), ([\w ]+)/g, "$2 $1"));
+
+function eatOne(match, amount, unit) {
+  amount = Number(amount) - 1;
+    if (amount == 1) {
+        unit = unit.slice(0, unit.length - 1);
+	  }
+	    else if (amount == 0) {
+	        unit = unit + "s";
+		    amount = "no";
+		      }
+		        return amount + " " + unit;
+			}
+
+			var stock = "1 lemon, 2 cabbages, and 101 eggs";
+			stock = stock.replace(/(\d+) (\w+)/g, eatOne);
+
+			print(stock);
