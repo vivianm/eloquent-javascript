2abff66edae85664eed109ddb7f5156040d03a7a
vivianm <vivianmbaji@gmail.com>
2013-11-30 16:34:55 +0300

    linking html file to js file

--- chapt12.js
+++ chapt12.js
@@ -19,3 +19,16 @@ function isImage(node) {
   }
 
   console.log(isImage(document.body.lastChild));
+
+function asHTML(node) {
+  if (isTextNode(node))
+      return escapeHTML(node.nodeValue);
+        else if (node.childNodes.length == 0)
+	    return "<" + node.nodeName + "/>";
+	      else
+	          return "<" + node.nodeName + ">" +
+		             map(asHTML, node.childNodes).join("") +
+			                "</" + node.nodeName + ">";
+					}
+
+					console.log(asHTML(document.body));
