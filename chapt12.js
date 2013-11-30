f563c04d81bf42d3212feaab117049e12d2812af
vivianm <vivianmbaji@gmail.com>
2013-11-30 14:47:51 +0300

    Adding properties called nextSibling and previousSibling, which point at the nodes sitting 'next' to a node

--- chapt12.js
+++ chapt12.js
@@ -3,3 +3,5 @@ console.log(document.body.parentNode);
 console.log(document.body.childNodes.length);
 console.log(document.documentElement.firstChild);
 console.log(document.documentElement.lastChild);
+console.log(document.body.previousSibling);
+console.log(document.body.nextSibling);
