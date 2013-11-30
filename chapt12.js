fe3c90c4afac862ece98586cfd2f0dc38b6cf4ed
vivianm <vivianmbaji@gmail.com>
2013-11-30 14:42:59 +0300

    Adding links called firstChild and lastChild, pointing at the first and last child inside a node

--- chapt12.js
+++ chapt12.js
@@ -1,3 +1,5 @@
 console.log(document.body);
 console.log(document.body.parentNode);
 console.log(document.body.childNodes.length);
+console.log(document.documentElement.firstChild);
+console.log(document.documentElement.lastChild);
