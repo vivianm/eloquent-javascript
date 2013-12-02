83f22975985563ef808bc0aedfcc990bca30dbdd
vivianm <vivianmbaji@gmail.com>
2013-12-02 18:02:41 +0300

    Add function makeTable

--- chapt12.js
+++ chapt12.js
@@ -121,3 +121,26 @@ function removeElement(node) {
             {Tree: "Coral", Flowers: "Red"},
 	               {Tree: "Pine",  Flowers: "None"}],
 		                 ["Tree", "Flowers"]);
+
+
+function makeTable(data, columns) {
+  var headRow = dom("TR");
+    forEach(columns, function(name) {
+        headRow.appendChild(dom("TH", null, name));
+	  });
+
+	    var body = dom("TBODY", null, headRow);
+	      forEach(data, function(object) {
+	          var row = dom("TR");
+		      forEach(columns, function(name) {
+		       row.appendChild(dom("TD", null, String(object[name])));
+			        });
+				    body.appendChild(row);
+				      });
+
+				        return dom("TABLE", null, body);
+					}
+
+		var table = makeTable(document.body.childNodes,
+					     ["nodeType", "tagName"]);
+				document.body.appendChild(table);
