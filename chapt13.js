00e966a88fd75be0040a0ccae91c33d821ffa075
vivianm <vivianmbaji@gmail.com>
2013-12-03 13:10:26 +0300

    Adding a click handler toa button in other browsers

--- chapt13.js
+++ chapt13.js
@@ -1,3 +1,6 @@
 attach(window.open("example_events.html"));
 
 $("button").attachEvent("onclick", function(){print("Click!");});
+
+$("button").addEventListener("click", function(){print("Click!");},
+                             false);
