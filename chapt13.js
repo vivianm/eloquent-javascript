6dea3eacd72cf0b1c04b36b2a45e51da3e807588
vivianm <vivianmbaji@gmail.com>
2013-12-03 16:48:39 +0300

    Adding normaliseEvent function

--- chapt13.js
+++ chapt13.js
@@ -79,3 +79,32 @@ function printKeyCode(event) {
 	     unregisterEventHandler($("textfield"), "keypress", printCharacter);
 
 
+function normaliseEvent(event) {
+  if (!event.stopPropagation) {
+      event.stopPropagation = function() {this.cancelBubble = true;};
+          event.preventDefault = function() {this.returnValue = false;};
+	    }
+	      if (!event.stop) {
+	          event.stop = function() {
+		        this.stopPropagation();
+			      this.preventDefault();
+			          };
+				    }
+
+				      if (event.srcElement && !event.target)
+				          event.target = event.srcElement;
+ if ((event.toElement || event.fromElement) && !event.relatedTarget)
+ event.relatedTarget = event.toElement || event.fromElement;
+ if (event.clientX != undefined && event.pageX == undefined) {
+event.pageX = event.clientX + document.body.scrollLeft;
+ event.pageY = event.clientY + document.body.scrollTop;
+							    }
+if (event.type == "keypress") {
+if (event.charCode === 0 || event.charCode == undefined)
+ event.character = String.fromCharCode(event.keyCode);
+ else
+									          event.character = String.fromCharCode(event.charCode);
+										    }
+
+										      return event;
+										      }
