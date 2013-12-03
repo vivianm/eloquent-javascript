fd3f6b6b7ddb08460f92471a8e6f3e481690dce1
vivianm <vivianmbaji@gmail.com>
2013-12-03 17:37:53 +0300

    Add HTTP object for to make a request

--- chapt14.js
+++ chapt14.js
@@ -10,3 +10,9 @@ function makeHttpObject() {
 	      }
 
 	      console.log(typeof(makeHttpObject()));
+
+
+var request = makeHttpObject();
+request.open("GET", "files/fruit.txt", false);
+request.send(null);
+console.log(request.responseText);
