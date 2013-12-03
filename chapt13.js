e516427fa3d9d58cef7dbac5ece522a7fa2860c2
vivianm <vivianmbaji@gmail.com>
2013-12-03 13:16:41 +0300

    Adding a click handler to a button in different browsers including Internet Explorer

--- chapt13.js
+++ chapt13.js
@@ -4,3 +4,13 @@ $("button").attachEvent("onclick", function(){print("Click!");});
 
 $("button").addEventListener("click", function(){print("Click!");},
                              false);
+
+function registerEventHandler(node, event, handler) {
+  if (typeof node.addEventListener == "function")
+      node.addEventListener(event, handler, false);
+        else
+	    node.attachEvent("on" + event, handler);
+	    }
+
+	    registerEventHandler($("button"), "click",
+	                         function(){print("Click (2)");});
