b47b3f0d04d70d91b65f4941bd45335786f7c4c1
vivianm <vivianmbaji@gmail.com>
2013-12-04 21:10:22 +0300

    Add a playerPos property used to determine where the player is trying to move

--- chapt13.js
+++ chapt13.js
@@ -238,3 +238,29 @@ var SokobanField = {
 																					};
 
 																					testField.place(document.body);
+
+
+																				SokobanField.move = function(direction) {
+																				  var playerSquare = this.getSquare(this.playerPos);
+																				    var targetPos = this.playerPos.add(direction);
+																				      var targetSquare = this.getSquare(targetPos);
+
+																				        // Possibly pushing a boulder
+																					  if (targetSquare.hasBoulder()) {
+																					      var pushTarget = this.getSquare(targetPos.add(direction));
+																					          if (pushTarget.isEmpty()) {
+																						        targetSquare.moveContent(pushTarget);
+																							    }
+																							        else if (pushTarget.isExit()) {
+																								      targetSquare.moveContent(pushTarget);
+																								            pushTarget.clearContent();
+																									          this.bouldersToGo--;
+																										        this.updateScore();
+																											    }
+																											      }
+																									 // Moving the player
+																								 if (targetSquare.isEmpty()) {
+																								 playerSquare.moveContent(targetSquare);
+																								 this.playerPos = targetPos;
+																													    }
+																													    };
