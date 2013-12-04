0255880c3899b02cb6f9a5cc3759f22ad6164c51
vivianm <vivianmbaji@gmail.com>
2013-12-04 20:22:40 +0300

    Adding a construct switch used to choose which code to execute based on some value

--- chapt14.js
+++ chapt14.js
@@ -132,3 +132,23 @@ function serializeJSON(value) {
 															          }
 																    }
 																    }
+
+
+
+										function weatherAdvice(weather) {
+										  switch(weather) {
+										      case "rainy":
+										            console.log("Remember to bring an umbrella.");
+											          break;
+												      case "sunny":
+												            console.log("Dress lightly.");
+													        case "cloudy":
+														      console.log("Go outside.");
+														            break;
+															        default:
+												 console.log("Unknown weather type: ", weather);
+																            break;
+																	      }
+																	      }
+
+													 weatherAdvice("sunny");
