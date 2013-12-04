b26aba0e2095e993d38baeff9e60ead6aa03d0ef
vivianm <vivianmbaji@gmail.com>
2013-12-04 18:44:14 +0300

    Add a continue loop

--- chapt14.js
+++ chapt14.js
@@ -113,3 +113,9 @@ function serializeJSON(value) {
 										  var answer = prompt("Say 'moo'.", "");
 										    console.log("You said '", answer, "'.");
 										    } while (answer != "moo");
+
+										for (var i = 0; i < 10; i++) {
+										  if (i % 3 != 0)
+										      continue;
+										        console.log(i, " is divisible by three.");
+											}
