ee4097f0bd6c7b27499a455d5413b79bffc86596
vivianm <vivianmbaji@gmail.com>
2013-12-04 18:35:37 +0300

    Add function serializeJSON that produces a string with the value's JSON representation

--- chapt14.js
+++ chapt14.js
@@ -47,3 +47,45 @@ function evalJSON(json) {
   }
   var fruit = evalJSON(request.responseText);
   console.log(fruit);
+
+
+
+
+
+function serializeJSON(value) {
+  function isArray(value) {
+      return /^\s*function Array/.test(String(value.constructor));
+        }
+
+	  function serializeArray(value) {
+	      return "[" + map(serializeJSON, value).join(", ") + "]";
+	        }
+		  function serializeObject(value) {
+		      var properties = [];
+		          forEachIn(value, function(name, value) {
+			        properties.push(serializeString(name) + ": " +
+				                      serializeJSON(value));
+						          });
+		 return "{" + properties.join(", ") + "}";
+							        }
+		function serializeString(value) {
+					var special =
+		{"\"": "\\\"", "\\": "\\\\", "\f": "\\f", "\b": "\\b",
+									           "\n": "\\n", "\t": "\\t", "\r": "\\r", "\v": "\\v"};
+										       var escaped = value.replace(/[\"\\\f\b\n\t\r\v]/g,
+										                                       function(c) {return special[c];});
+														           return "\"" + escaped + "\"";
+															     }
+
+															       var type = typeof value;
+													 if (type == "object" && isArray(value))
+														return serializeArray(value);
+																 else if (type == "object")
+													  return serializeObject(value);
+														 else if (type == "string")
+														 return serializeString(value);
+																		   else
+														 return String(value);
+																		       }
+
+														 print(serializeJSON(fruit));
