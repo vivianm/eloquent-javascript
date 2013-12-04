eb6d08856e477850129c42cee14391fe1ddb6bf9
vivianm <vivianmbaji@gmail.com>
2013-12-04 20:43:12 +0300

    convenient wrappers for registerEventHandler and unregisterEventHandler

--- chapt13.js
+++ chapt13.js
@@ -108,3 +108,20 @@ if (event.charCode === 0 || event.charCode == undefined)
 
 										      return event;
 										      }
+
+										function addHandler(node, type, handler) {
+										  function wrapHandler(event) {
+										      handler(normaliseEvent(event || window.event));
+										        }
+											  registerEventHandler(node, type, wrapHandler);
+											    return {node: node, type: type, handler: wrapHandler};
+											    }
+
+											    function removeHandler(object) {
+											      unregisterEventHandler(object.node, object.type, object.handler);
+											      }
+
+											  var blockQ = addHandler($("textfield"), "keypress", function(event) {
+											        if (event.character.toLowerCase() == "q")
+												    event.stop();
+												    });
