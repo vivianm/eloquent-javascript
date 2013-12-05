024bc9af3063d03f61ab7cb8e03723497178ea18
vivianm <vivianmbaji@gmail.com>
2013-12-05 10:50:17 +0300

    Adding a simple test

--- chapt14.js
+++ chapt14.js
@@ -29,3 +29,11 @@ request.onreadystatechange = function() {
   if (request.readyState == 4)
       show(request.responseText.length);
       };
+
+var heap = new BinaryHeap(function(x){return x;});
+forEach([10, 3, 4, 8, 2, 9, 7, 1, 2, 6, 5],
+        method(heap, "push"));
+
+	heap.remove(2);
+	while (heap.size() > 0)
+	  console.log(heap.pop());
