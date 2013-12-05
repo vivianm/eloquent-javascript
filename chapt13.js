0a35c2319353a7bda7dad475fa2366d8bd2fdc63
vivianm <vivianmbaji@gmail.com>
2013-12-05 10:40:36 +0300

    Adding setInterval to handle the timing of the animation

--- chapt13.js
+++ chapt13.js
@@ -342,3 +342,23 @@ SokobanGame.keyDown = function(event) {
 									          }
 										    }
 										    };
+
+
+
+Square.clearContent = function() {
+  self.content = null;
+    var image = this.tableCell.lastChild;
+      var size = 100;
+
+        var animate = setInterval(function() {
+	    size -= 10;
+	        image.style.width = size + "%";
+		    image.style.height = size + "%";
+
+		        if (size < 60) {
+			      clearInterval(animate);
+			            removeElement(image);
+				        }
+					  }, 70);
+					  };
+									        
